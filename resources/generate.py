ce6f64be93a15996492c21aec741f3f73d5b9a8a
LpSamuelm <powpowd@gmail.com>
2013-05-15 23:58:10 +0200

    Improved documentation and effectivized
    addUn() is now faster, and generate.py is more well-documented.

--- resources/generate.py
+++ resources/generate.py
@@ -9,25 +9,25 @@ import startops  ## Initializing program with variables and such
 
 ## -- The next few lines sets startops variables -- ##
 
-swears          =   startops.swears
-aliases         =   startops.aliases
-ignore          =   startops.ignore
-minlength       =   startops.minlength
-ignored_words   =   startops.ignored_words
-logformat       =   startops.logformat
-pathoverride    =   startops.pathoverride
-logdirs         =   startops.logdirs
-lognames        =   startops.lognames
-printprogress   =   startops.printprogress
+swears          =   startops.swears         ## Options
+aliases         =   startops.aliases        ##
+ignore          =   startops.ignore         ##
+minlength       =   startops.minlength      ##
+ignored_words   =   startops.ignored_words  ##
+logformat       =   startops.logformat      ##
+pathoverride    =   startops.pathoverride   ##
+logdirs         =   startops.logdirs        ##
+lognames        =   startops.lognames       ##
+printprogress   =   startops.printprogress  ##
 
-pattern_username    =   startops.pattern_username
-pattern_useraction  =   startops.pattern_useraction
-pattern_saidwords   =   startops.pattern_saidwords
-pattern_actionwords =   startops.pattern_actionwords
-pattern_time        =   startops.pattern_time
+pattern_username    =   startops.pattern_username       ## Regexes for line matching
+pattern_useraction  =   startops.pattern_useraction     ##
+pattern_saidwords   =   startops.pattern_saidwords      ##
+pattern_actionwords =   startops.pattern_actionwords    ##
+pattern_time        =   startops.pattern_time           ##
 
-hnum            =   startops.hnum
-mnum            =   startops.mnum
+hnum            =   startops.hnum   ## Time format help
+mnum            =   startops.mnum   ##
 
 ## ------------------------------------------------ ##
 
@@ -41,49 +41,54 @@ def nofunc(*args): ## Here as a placeholder function
     pass
 
 def compareNames(poster, linenums):
-    for comparison in linenums:
+    """Matches usernames regardless of case."""
+    if poster in linenums:
+        return poster
+    for comparison in linenums: ## Checks in existing name dictionary
         if poster.lower() == comparison.lower():
             return comparison
     return False
 
 def hasAlias(name, adict=aliases):
-    for comparison in aliases:
+    """Compares 'name' to names in adict."""
+    for comparison in adict: ## Checks in existing aliases dictionary
         if name.lower() in [a.lower() for a in adict[comparison]]:
             return comparison
     return False
 
 def compareAndAlias(poster, linenums):
+    """Shorthand function for both compareNames and hasAlias."""
     comparison = compareNames(poster, linenums)
-    if comparison:
+    if comparison: ## Using compareNames
         return comparison
     comparison = hasAlias(poster)
-    if comparison:
+    if comparison: ## Using hasAlias
         return comparison
     return False
 
-def addUn(poster, linenums):
+def addUn(poster, linenums, check=False):
+    """Returns 'linenums' with the appropriate key for 'poster' updated."""
     try:
-        linenums[poster] += 1
+        linenums[poster] += 1 ## Poster already exists in linenums
     except KeyError:
-        comparison = compareNames(poster, linenums)
-        if comparison:
-            linenums[comparison] += 1
-        else:
-            alias = hasAlias(poster)
-            if alias:
+        if check:
+            comparison = compareAndAlias(poster, linenums) ## Checks for aliases
+            if comparison:
                 try:
-                    linenums[alias] += 1
+                    linenums[comparison] += 1 ## Comparison exists
                 except KeyError:
-                    linenums[alias] = 1
+                    linenums[comparison] =  1 ## Comparison does not exist
             else:
-                linenums[poster] = 1
-    return linenums
+                linenums[poster] = 1 ## Poster has no alias and is new to linenums    return linenums
+        else:
+            linenums[poster] = 1
 
 class Logs(object):
+    """Handles IRC logs, and the generation of Python-friendly statistics based on them."""
     def __init__(self, directory, logs, printprogress=False):
         self.directory = directory
         self.logs = logs
-        if type(logs) == list:
+        if type(logs) == list: ## Adds some freedom/error handling to the config
             self.paths = [directory + log for log in self.logs]
         else:
             self.paths = [directory + log]
@@ -110,53 +115,64 @@ class Logs(object):
 
     ## ---
 
-    find_username       =   re.compile(pattern_username     )
-    find_useraction     =   re.compile(pattern_useraction   )
-    find_saidwords      =   re.compile(pattern_saidwords    )
-    find_actionwords    =   re.compile(pattern_actionwords  )
-    find_time           =   re.compile(pattern_time         )
+    find_username       =   re.compile(pattern_username     ) ## Regex patterns
+    find_useraction     =   re.compile(pattern_useraction   ) ##
+    find_saidwords      =   re.compile(pattern_saidwords    ) ##
+    find_actionwords    =   re.compile(pattern_actionwords  ) ##
+    find_time           =   re.compile(pattern_time         ) ##
 
     def countLines(self, special=nofunc):
-        for log in self.paths:
+        """Generates stats for number of lines per user, and executes 'special' while it's at it."""
+        for log in self.paths: ## Counts things in every supplied log
             infile = open(log, 'r')
             for line in infile:
                 
-                self.totallines += 1
-                
-                un          =   re.search(self.find_username, line      )
-                ua          =   re.search(self.find_useraction, line    )
-                unw         =   re.search(self.find_saidwords, line     )
-                uaw         =   re.search(self.find_actionwords, line   )
+                self.totallines += 1 ## Counts every line in the log. Might want to effectivize this somehow
 
-                linetime    =   re.search(self.find_time, line          )
+                ## -Creates search objects based on earlier patterns- ##
+                un          =   re.search(self.find_username, line      ) ## Username
+                ua          =   re.search(self.find_useraction, line    ) ## Username in actions
+                unw         =   re.search(self.find_saidwords, line     ) ## Said words
+                uaw         =   re.search(self.find_actionwords, line   ) ## Action words
+                linetime    =   re.search(self.find_time, line          ) ## Time of line
+                ## -------------------------------------------------- ##
+                
                 if linetime:
                     linetime    =   (
-                                        linetime.group(hnum),
-                                        linetime.group(mnum),
+                                        linetime.group(hnum), ## Allows for different time formats
+                                        linetime.group(mnum), ##
                                     )
                 
                 if un and un.group(1).lower().strip() not in [x.lower() for x in ignore]:
+                    ## Only does anything at all if there's a valid, non-ignored username
                     u = compareAndAlias(un.group(1), self.linenums)
-                    if not u:
+                    if not u: ## For users without alias
                         u = un.group(1)
-                    special(u, unw.group(1), '', linetime)
+                    special(u, unw.group(1), '', linetime) ## Executes special functions
+                    ## Special argument format:
+                    ## Username, line, prefix, time of line
                     self.linenums = addUn(un.group(1), self.linenums)
                 elif ua and ua.group(1).lower().strip() not in [x.lower() for x in ignore]:
+                    ## Only does anything at all if there's a valid, non-ignored username
                     u = compareAndAlias(ua.group(1), self.linenums)
-                    if not u:
+                    if not u: ## For users without alias
                         u = ua.group(1)
-                    special(u, uaw.group(1), ua.group(1) + ' ', linetime)
+                    special(u, uaw.group(1), ua.group(1) + ' ', linetime) ## Executes special functions
+                    ## Special argument format:
+                    ## Username, line, prefix, time of line
                     self.uactions = addUn(ua.group(1), self.uactions)
-            uactions_org = self.uactions.copy()
-            for comparison in uactions_org:
+            uactions_org = self.uactions.copy() ## Creates iterable dictionary, original will be modified
+
+            for comparison in uactions_org: ## Adds user action lines to total line numbers for said user
                 c = compareAndAlias(comparison, self.linenums)
                 if c:
                     self.linenums[c] += self.uactions[comparison]
-                else:
+                else: ## Makes sure no ChanServs or such make it into the stats
                     self.actionsonly[comparison] = self.uactions.pop(comparison, {'NotWorking': 1})
 
-    def listLines(self, username, line, t) :
-        line = line.strip()
+    def listLines(self, username, line, t): ## Could theoretically merge this with addUn
+        """Assigns every user a list of every line they've said."""
+        line = line.strip() ## No newlines or unnecessary whitespace
         try:
             self.userlines[username].append(t + line)
             return
@@ -170,13 +186,15 @@ class Logs(object):
             else:
                 self.userlines[username] = [t + line]
 
-    def fixListLines(self):
+    def fixListLines(self): ## Prevents ChanServ-like bots from getting into the statistics
+        """Makes sure action-only users don't get line lists."""
         userlines_org = self.userlines.copy()
         for l in userlines_org:
             if l not in self.linenums:
                 del self.userlines[l]
                 
     def countWords(self, username, words, t):
+        """Counts words and assigns number to user."""
         for w in words:
             if len(w) >= minlength and\
             w.lower() not in [word.lower() for word in ignored_words] and\
@@ -188,19 +206,21 @@ class Logs(object):
                     self.wordnums[w.lower()] = 1
 
     def countSwears(self, username, words):
+        """Counts swears and assigns number to user."""
         for w in words:
             for s in swears:
-                bob = re.compile("^" + s + "$", re.IGNORECASE)
+                bob = re.compile("^" + s + "$", re.IGNORECASE)  ## Master of variable names
                 if re.search(bob, w.lower()):
                     try:
                         self.swears[username][w.lower()] += 1
-                    except KeyError:
+                    except KeyError: ## Standard key creation
                         try:
                             self.swears[username][w.lower()] = 1
                         except KeyError:
                             self.swears[username] = {w.lower(): 1}
 
     def commonWords(self):
+        """Counts how many times every word was used"""
         for w in self.wordnums:
             for number, pair in enumerate(self.wordlist):
                 if self.wordnums[w] >= pair[1]:
@@ -211,14 +231,17 @@ class Logs(object):
         del self.wordlist[-1]
 
     def timeCount(self, time):
+        """Adds 1 to the time dictionary for every line that was posted during that hour."""
         self.times[int(time[0])] += 1
 
     def returnWords(self, username, line):
-        if line[:len(username)].lower() != username.lower():
-            return re.split("[;:\(\)@\.\s\!\?\",]+", line)
-        return re.split("[;:\(\)@\.\s\!\?\",]+", line[len(username):])
+        """Gets a list of the words used in 'line'."""
+        if line[:len(username)].lower() != username.lower(): ## Doesn't count actions' first words - the usernames
+            return re.split("[;:\(\)@\.\s\!\?\",]+", line)              ## Splits at any non-letter
+        return re.split("[;:\(\)@\.\s\!\?\",]+", line[len(username):])  ## ... or mid-word delimiter
 
     def specialFuncs(self, username, line, prefix, time):
+        """Does a multitude of special functions."""
         words = self.returnWords(username, line)
         self.countWords(username, words, prefix)
         self.countSwears(username, words)
@@ -226,11 +249,13 @@ class Logs(object):
         self.timeCount(time)
     
     def randLines(self):
+        """Chooses a random line for each user."""
         for user in self.userlines:
             randtemp = random.choice(self.userlines[user])
             self.randomlines[user] = randtemp
 
     def genEverything(self):
+        """Generates all stats. Used in __init__."""
         if self.logs:
             if self.printprogress:
                 print "Scanning lines..."
