30e62e559df270a411e67ace7af361e22acf1859
LpSamuelm <powpowd@gmail.com>
2013-05-19 04:06:34 +0200

    Output test, more Unicode fixes
    You can now, with output-test.py, output a file to display your newly
    generated statistics. It's still nowhere near complete, but at least you
    can have your output in somewhat tangible form.
    
    Updated readme.md to reflect this.

---
+++ output-test.py
@@ -0,0 +1,42 @@
+# -*- coding: UTF-8 -*-
+
+import sys
+import os
+sys.path.insert(0, os.getcwd() + "\\resources")
+from generate import *
+import time
+
+x = time.time()
+check = Logs('', ['testlog.log'], True)
+y = time.time()
+superfile = open('output\\index.html', 'w')
+superfile.write('<link rel="stylesheet" type="text/css" href="stylesheet.css"><p>')
+superfile.close()
+superfile = open('output\\index.html', 'a')
+superfile.write("Total number of lines: " + str(check.totallines))
+superfile.write("<br>\n<br>\nTotal number of lines by user:<br>\n")
+for u in check.linenums:
+    superfile.write(u + ': ' + str(check.linenums[u]) + " lines, ")
+superfile.write("<br>\n<br>\nUsers that never spoke, only used actions (and their number of lines):<br>\n")
+for u in check.actionsonly:
+    superfile.write(u + ': ' + str(check.actionsonly[u]) + " lines, ")
+superfile.write("<br>\n<br>\nTotal number of actions by user:<br>\n")
+for u in check.uactions:
+    superfile.write(u + ': ' + str(check.uactions[u]) + " actions, ")
+superfile.write("<br>\n<br>\nRandom lines from each user:")
+for user in check.randomlines:
+    superfile.write("<br>\n  " + user.encode('utf-8') + ": " + check.randomlines[user].encode('utf-8'))
+superfile.write("<br>\n<br>\nMost common words in the channel, and how often they were used:<br>\n")
+for word in check.wordlist[:15]:
+    superfile.write(word[0] + ': ' + str(word[1]) + " times, ")
+superfile.write("<br>\n<br>\nHow often people swore with each swear:")
+for u in check.swears:
+    superfile.write('<br>\n<br>\n' + u + ':')
+    for swear in check.swears[u]:
+        superfile.write('<br>\n\t&emsp;' + swear.encode('utf-8') + ': ' + str(check.swears[u][swear]).encode('utf-8') + " times")
+superfile.write("<br>\n<br>\nHow often people spoke during each hour:<br>\n")
+for time in check.times:
+    superfile.write(str(time) + " o' clock: " + str(check.times[time]) + " lines, ")
+superfile.write('<br><br><font size="1">These statistics were generated in ' + str(round(y - x, 2)).encode('utf-8') + " seconds.</font>")
+
+superfile.close()
--- readme.md
+++ readme.md
@@ -6,13 +6,18 @@ It will take log files (in different formats, and you will eventually be able to
 It will not require any specific input past setup, so it will easily be able to be scheduled without complications.
 
 ##Usage
-Currently, the program cannot be used to generate any HTML files. If you'd really like to, you can supply a log file and see the results in Python-compliant form using `gen-test.py`, if you rename it to `testlog.log` and put it in the PYCS directory. The script doing all the work is `generate.py`.
+Currently, the program cannot be used to generate any HTML files. If you'd really like to, you can supply a log file and see the results in a kind of slightly maybe visually pleasing form using `gen-test.py` or `output-test.py`, if you rename it to `testlog.log` and put it in the PYCS directory. The script doing all the work is `generate.py`.
 
 In order to create the config files without generating any stats, run `startops.py` in `resources`. This allows you to modify `settings.cfg` and `aliases.cfg` before the first run.
 
 You can also try using what will be the main program - `pycs.py` - with the correct settings. Without using `Path override`, this currently only works on Windows, but if you supply your own paths it should work just fine.
 
 You can create your own log format parser easily using the instructions in `\resources\formats\creation.txt`.
+
+###`output-test.py`
+This script will output a simple file with the filename `index.html` in the `output` directory, based on the log files you've specified in `settings.cfg`. This is at the moment very rudimentary and doesn't produce a very pretty result at all, but it works!
+
+In order to make use of the output file, you can easily set up a `.bat` or `.sh` to upload it to your site. If you'd like to add some style, you can supply your own `stylesheet.css` in the same directory as the output file.
 
 ###Oh, and...
 I'm not really known for how good I am at finishing what I've started. There's always the risk I will abandon this project, and in several years look back on it and cry.
